5e5242fc68ea6a915981f0b2af317f2d190c55bc
Scott Straley <scottstraley@gmail.com>
2017-06-28 05:52:47 +0000

    got option-blah working

--- server.js
+++ server.js
@@ -31,13 +31,13 @@ app.get( "/api/load/:filename*", function( req, res ) {
 
 /* serves main page */
 app.get("/", function(req, res) {
-  res.sendfile('index.html')
+  res.sendfile('./public/index.html')
 });
 
 /* serves all the static files */
 app.get(/^(.+)$/, function(req, res){ 
   console.log('static file request : ' + req.params);
-  res.sendfile( __dirname + req.params[0]); 
+  res.sendfile( __dirname + "/public/" + req.params[0]); 
 });
 
 var port = 9000;
